920f0d55eeea886f3978ea8deffdab53febe3e6f
Henri Lehtinen <henri.a.lehtinen@outlook.com>
2021-02-19 18:15:38 +0200

    Project starting point create-react-app and start.spring.io code loaded

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import ListItems from './components/list-items';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ListItems/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
